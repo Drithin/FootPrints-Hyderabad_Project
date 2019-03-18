2ffe4273804955af0d736add7fd606d0b22aa098
Drithin Chakravarthy Nellore <drithn@gmail.com>
2019-03-18 17:56:46 +0530

    added script for contact form validation

--- BootStrap-Version-2/script.js
+++ BootStrap-Version-2/script.js
@@ -14,3 +14,45 @@ function myFunction() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
+
+//contact form validation
+  $("form").submit(function(e) {
+              
+              var error = "";
+
+               if ($("#name").val() == "") {
+                  
+                  error += "The name field is required.<br>"
+                  
+              }
+              
+              if ($("#email").val() == "") {
+                  
+                  error += "The email field is required.<br>"
+                  
+              }
+              
+              if ($("#subject").val() == "") {
+                  
+                  error += "The subject field is required.<br>"
+                  
+              }
+              
+              if ($("#content").val() == "") {
+                  
+                  error += "The content field is required.<br>"
+                  
+              }
+              
+              if (error != "") {
+                  
+                 $("#error").html('<div class="alert alert-danger" role="alert"><p><strong>There were error(s) in your form:</strong></p>' + error + '</div>');
+                  
+                  return false;
+                  
+              } else {
+                  
+                  return true;
+                  
+              }
+          })
