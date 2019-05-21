6afa5a6057dafbf3bcda935887250f4054f3cb50
Drithin Chakravarthy Nellore <drithn@gmail.com>
2019-05-21 12:48:19 +0530

    Selected and changed properties in css of herobox with jquery selectors and coded a function to call it on reload. Added fade in effect to h1 on herobox

--- Version-4/script.js
+++ Version-4/script.js
@@ -2,3 +2,19 @@ $( '#navbarNavDropdown .navbar-nav a' ).on( 'click', function () {
 	$( '#navbarNavDropdown .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
 	$( this ).parent( 'li' ).addClass( 'active' );
 });
+
+$('h1').fadeToggle(5000);
+
+
+var totalCount = 4;
+
+function ChangeIt(){
+
+	var num = Math.ceil( Math.random() * totalCount );
+
+	$('.herobox').css('background-image', 'url('+num+'.jpeg');
+	
+	console.log($('.herobox').background = num+'.jpeg');
+		
+}
+
